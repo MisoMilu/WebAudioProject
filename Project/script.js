52d71b65000317ed072d4994c50ed03c4cfc7057
Samantha <samanthachang01@gmail.com>
2026-09-06 23:54:10 -0400

    drawinmg using ctx

--- Project/script.js
+++ Project/script.js
@@ -8,7 +8,12 @@ const canvas = document.getElementById('canvas1');
 // A drawing context is the object that gives you the tools to draw onto a canvas.
 const ctx = canvas.getContext('2d') // Hey, give me tools to draw 2D things on Canvas
 
-//3. Check if canvas is succesfully in showing in browser
+//3. Check if canvas is succesfully in showing in browser if its console.log('canvas')
 //3. Chek if Javascripts has succesfully found canvas. -> need to go to browswer and inspect, then hit console 
 // to see that java prints out the value in the console
+//3.5 Prints the 2D drawing tools (ctx) to the Console, so you can confirm they were created.
 console.log(ctx);
+
+// try to draw using ctx and see if it appears on the screen
+ctx.fillStyle = 'white'
+ctx.fillRect(10,20,150,50);
